8f0d571f4e877a0113d606170b9b2ee1efb79475
WebRizen <135026315+Webrizen@users.noreply.github.com>
2025-09-27 10:57:12 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
+const allowedOrigins = ['https://unified-booking-platform.vercel.app', 'http://localhost:3000', 'https://unify-book.vercel.app']
+ 
+const corsOptions = {
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
 export async function middleware(request) {
+   // Check the origin from the request
+  const origin = request.headers.get('origin') ?? ''
+  const isAllowedOrigin = allowedOrigins.includes(origin)
+if (isAllowedOrigin) {
+    response.headers.set('Access-Control-Allow-Origin', origin)
+  }
+  
   const token = request.cookies.get('token')?.value;
 
   if (request.nextUrl.pathname.startsWith('/admin')) {
